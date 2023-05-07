4ee16507c0a97c6a76b5e723424ca85f4c066884
Dimcom1010 <dimcom1010@gmail.com>
2023-05-07 17:33:46 +0300

    featch: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const newPath = path.join(__dirname, 'files-copy')
+
+fs.mkdir(newPath, () => {
+    fs.readdir(
+        path.join(__dirname, 'files'),
+        (err, files) => {
+            err ? console.log('err1', err) : null;
+            files.forEach(e => {
+                const pathFile = path.join(__dirname, 'files', e)
+                const name = path.basename(pathFile)
+
+                fs.readFile(
+                    pathFile,
+                    'utf8',
+                    (err, data) => {
+                        err ? console.log('err2', err) : null;
+
+                        fs.writeFile(
+                            path.join(newPath, name),
+                            data,
+                            (err) => {
+                                err ? console.log('err2', err) : null;
+                            }
+                        )
+                    }
+                )
+
+            }
+            )
+
+        })
+})
